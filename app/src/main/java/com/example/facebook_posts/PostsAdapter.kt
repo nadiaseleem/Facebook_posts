5e70ac11898579129c8a4580b5a3ac3e0505f81f
ziad328 <Ziad32seleem@gmail.com>
2024-09-03 06:05:36 +0300

    % code refactoring

--- app/src/main/java/com/example/facebook_posts/PostsAdapter.kt
+++ app/src/main/java/com/example/facebook_posts/PostsAdapter.kt
@@ -6,8 +6,15 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.example.facebook_posts.databinding.PostItemBinding
 
-class PostsAdapter(val posts: List<Post>) : Adapter<PostsAdapter.ViewHolder>() {
-    inner class ViewHolder(val binding: PostItemBinding) : RecyclerView.ViewHolder(binding.root)
+class PostsAdapter(private val posts: List<Post>) : Adapter<PostsAdapter.ViewHolder>() {
+    inner class ViewHolder(private val binding: PostItemBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(post: Post){
+            binding.personNameTv.text = post.author
+            binding.dateTv.text = post.date
+            binding.postTv.text = post.content
+            binding.postImg.setImageResource(post.image)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -23,9 +30,7 @@ class PostsAdapter(val posts: List<Post>) : Adapter<PostsAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val post = posts[position]
-        holder.binding.personNameTv.text = post.author
-        holder.binding.dateTv.text = post.date
-        holder.binding.postTv.text = post.content
-        holder.binding.postImg.setImageResource(post.image)
+        holder.bind(post)
     }
+
 }
